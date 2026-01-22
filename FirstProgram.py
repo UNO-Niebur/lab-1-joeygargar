3ad2f71776372c5696b1e0427951eac96820c60d
joeygargar <miagarcia@unomaha.edu>
2026-01-21 21:12:03 -0600

    Fix indentation and update print statements

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,16 +6,17 @@
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
-
+  username = input("what is your name?")
   #Use the user's name in the program.
-
+  print("nice to meet you", username)
   #Ask the user for their age.
-
+  userage = input("how old are you?")
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
-
+  birthyear = 2026 - userage 
+  print("you were born in", birthyear) 
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
