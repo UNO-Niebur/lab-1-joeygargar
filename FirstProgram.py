4b979bac20338e6d56a4b2d70413076763f6e501
joeygargar <miagarcia@unomaha.edu>
2026-01-22 21:37:06 -0600

    Completed Lab 1

--- FirstProgram.py
+++ FirstProgram.py
@@ -12,7 +12,7 @@ def main():
   #Use the user's name in the program.
   print("nice to meet you", username)
   #Ask the user for their age.
-  userage = int(input("how old are you?"))
+  userage = int(input("how old are you?(as a number)"))
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
   birthyear = 2026 - userage 
--- MadLib.py
+++ MadLib.py
@@ -1,15 +1,22 @@
 #MadLib.py
 #Name: Mia Garcia
-#Date: 1/21/26
+#Date: 1/22/26
 #Assignment: Lab 1 
 
 def main():
   print("Lets Do a Madlib!")
   #Ask user for words
-  Noun1 = input("Enter a Noun: ")
-  Adjective1 = input("Enter a Adjective: ")
+  noun1 = input("Enter a noun: ")
+  noun2 = input("Enter a noun: ")
+  place1 = input("Enter a place: ")
+  adjective1 = input("Enter a adjective: ")
+  verb1 = input ("Enter a verb: ")
+  adjective2 = input("Enter a adjective: ")
   #Print the story with the user supplied words.
-  print("This is the noun: ", Noun1)
+  print("Everybody hold on to your", noun1, "the", noun2,
+  "is about to take off. Please remain in your seats and keep your seat belts fastened as we make our way to", place1,
+  ". This ride will be", adjective1,"so make sure your ready to", verb1,
+  ". We apologize for the", adjective2,"smell and hope you enjoy the ride." )
 
 
 #Call the main function if this is the file being run.
